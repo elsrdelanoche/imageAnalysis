91fa1179801d6fcfb8e3d984f094684d8885f3e1
elsrdelanoche <alfredobr587@gmail.com>
2025-11-11 23:50:29 -0600

    Add practica 8s

---
+++ Practica_8_FiltradoImagenesDeteccionBordes/build/CMakeFiles/filtros.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/alfredo/Documents/imageAnalysis/FiltrosBordesGTK3/src/controller/AppController.cpp" "CMakeFiles/filtros.dir/src/controller/AppController.cpp.o" "gcc" "CMakeFiles/filtros.dir/src/controller/AppController.cpp.o.d"
+  "/home/alfredo/Documents/imageAnalysis/FiltrosBordesGTK3/src/main.cpp" "CMakeFiles/filtros.dir/src/main.cpp.o" "gcc" "CMakeFiles/filtros.dir/src/main.cpp.o.d"
+  "/home/alfredo/Documents/imageAnalysis/FiltrosBordesGTK3/src/model/ImageModel.cpp" "CMakeFiles/filtros.dir/src/model/ImageModel.cpp.o" "gcc" "CMakeFiles/filtros.dir/src/model/ImageModel.cpp.o.d"
+  "/home/alfredo/Documents/imageAnalysis/FiltrosBordesGTK3/src/utils/Canny.cpp" "CMakeFiles/filtros.dir/src/utils/Canny.cpp.o" "gcc" "CMakeFiles/filtros.dir/src/utils/Canny.cpp.o.d"
+  "/home/alfredo/Documents/imageAnalysis/FiltrosBordesGTK3/src/utils/Convolution.cpp" "CMakeFiles/filtros.dir/src/utils/Convolution.cpp.o" "gcc" "CMakeFiles/filtros.dir/src/utils/Convolution.cpp.o.d"
+  "/home/alfredo/Documents/imageAnalysis/FiltrosBordesGTK3/src/utils/ImgUtils.cpp" "CMakeFiles/filtros.dir/src/utils/ImgUtils.cpp.o" "gcc" "CMakeFiles/filtros.dir/src/utils/ImgUtils.cpp.o.d"
+  "/home/alfredo/Documents/imageAnalysis/FiltrosBordesGTK3/src/utils/Kernels.cpp" "CMakeFiles/filtros.dir/src/utils/Kernels.cpp.o" "gcc" "CMakeFiles/filtros.dir/src/utils/Kernels.cpp.o.d"
+  "/home/alfredo/Documents/imageAnalysis/FiltrosBordesGTK3/src/view/MainWindow.cpp" "CMakeFiles/filtros.dir/src/view/MainWindow.cpp.o" "gcc" "CMakeFiles/filtros.dir/src/view/MainWindow.cpp.o.d"
+  "" "filtros" "gcc" "CMakeFiles/filtros.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Practica_8_FiltradoImagenesDeteccionBordes/build/CMakeFiles/filtros.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for filtros.
+# This may be replaced when dependencies are built.
