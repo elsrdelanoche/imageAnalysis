df08e402c90506565cbda2ae9cb6d1fe480e0779
Alfredo Bautista <139198090+elsrdelanoche@users.noreply.github.com>
2025-12-28 23:35:03 -0600

    Delete test8/Practica_8_FiltradoImagenesDeteccionBordes directory

--- test8/Practica_8_FiltradoImagenesDeteccionBordes/src/utils/Canny.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <vector>
-namespace Canny {
-struct Params { int k_gauss=5; double sigma=1.0; double t_low=0.1; double t_high=0.2; };
-std::vector<double> Run(const std::vector<double>& img, int w, int h, const Params& p);
-}
--- test8/Practica_8_FiltradoImagenesDeteccionBordes/src/utils/ImgUtils.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <gtkmm.h>
-#include <vector>
-namespace ImgUtils {
-std::vector<double> PixbufToGray(const Glib::RefPtr<Gdk::Pixbuf>& p);
-Glib::RefPtr<Gdk::Pixbuf> GrayToPixbuf(const std::vector<double>& g, int w, int h);
-inline double clamp01(double v){ return v<0?0:(v>1?1:v); }
-double sample_reflect(const std::vector<double>& img, int w, int h, int x, int y);
-}
